0262aef20e2584f6550b3ca842dcdab769a1c635
DerkachDiana <89642137278@mail.ru>
2022-10-25 12:53:19 +0300

    Set up font to Login Page

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   project: {
     ios: {},
-    android: {}, // grouped into "project"
+    android: {},
   },
-  assets: ['./src/assets/font/'], // stays the same
+  assets: ['./src/assets/font/'],
 };
