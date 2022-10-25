7b324e4a245a94ec4083628aad1cc759598e42d7
DerkachDiana <89642137278@mail.ru>
2022-10-25 12:26:17 +0300

    Set up font to Login Page

--- src/components/title/styles.ts
+++ src/components/title/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native';
 export const MainTitle = styled.Text`
   font-size: 40px;
   color: ${(props) => props.color};
-  font-weight: bold;
+  font-family: 'playfair_display_regular';
 `;
